3e03a50b7dfec8986d6ea11a3ed2c5fec9e6ce57
Jeffrey Yi <jeffreyyi11@gmail.com>
2021-06-29 12:13:30 -0700

    add gitignore for key

--- laced/settings.py
+++ laced/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@%jk4++9irldfl4z((gew1g(yz)vi7wlkktja&lpzpfr301=tu'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
